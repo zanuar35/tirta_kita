ab2cf9337e956901407329d94e6cb8d8a8ad8488
Zanuar Ragmat <rahmatzanuar35@gmail.com>
2021-06-10 15:17:04 +0700

    initial commit

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tirta_kita/constants.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +9,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.blue[200],
+      // backgroundColor: Colors.blue[200],
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             height: size.height * 0.30,
@@ -29,15 +32,15 @@ class HomeScreen extends StatelessWidget {
                     left: 0,
                     right: 0,
                     child: Container(
-                      height: 65,
+                      height: 80,
                       margin: EdgeInsets.symmetric(horizontal: 24),
                       decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(8),
                           boxShadow: [
                             BoxShadow(
                                 offset: Offset(0, 10),
-                                blurRadius: 70,
+                                blurRadius: 25,
                                 color: Colors.black.withOpacity(0.4))
                           ]),
                     ))
@@ -47,14 +50,49 @@ class HomeScreen extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          Container(
-            color: Colors.red,
+          SizedBox(
             height: 20,
-            width: 100,
+          ),
+          Container(
+            height: 120,
+            child: ListView.builder(
+                itemCount: 6,
+                padding: EdgeInsets.only(left: 15, right: 15),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: Column(
+                      children: [
+                        Card(
+                          elevation: 4,
+                          shape: CircleBorder(),
+                          child: Container(
+                            height: 90,
+                            width: 90,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text('Kategori')
+                      ],
+                    ),
+                  );
+                }),
           ),
           SizedBox(
             height: 20,
           ),
+          Text(
+            "Daftar Promo",
+            style: GoogleFonts.rubik(
+                textStyle:
+                    TextStyle(fontWeight: FontWeight.w700, fontSize: 13)),
+          )
         ],
       ),
     );
